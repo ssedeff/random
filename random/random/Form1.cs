38cf62f5c8156295cffd813358ab9c81e9d6cf13
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-17 16:23:47 +0300

    Add files via upload

---
+++ random/random/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int elemansayisi=listBox1.Items.Count;
+            Random rnd=new Random();
+            int secim =rnd.Next(elemansayisi);
+            listBox1.SelectedIndex = secim;
+        }
+    }
+}
